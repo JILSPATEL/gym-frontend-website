4c65a02ef0043c928a1a1006a69534c1bb133d62
JILS PATEL <100358865+JILSPATEL@users.noreply.github.com>
2023-07-31 08:15:00 +0530

    Add files via upload

---
+++ GYM.php
@@ -0,0 +1,19 @@
+<?php
+    $Name=$_POST['name'];
+    $Age=$_POST['age'];
+    $Address=$_POST['adr'];
+    $Number=$_POST['num'];
+    
+    $conn = new mysqli('localhost','root','','gym_db');
+    if($conn->connect_error){
+        die('connection failed :'+$conn->connect_error);
+}
+else{
+    $stmy=$conn->prepare("insert into gym1(Name,Age,Address,Number) values(?,?,?,?)");
+    $stmy->bind_param("ssss",$Name,$Age,$Address, $Number);
+    $stmy->execute();
+    echo"register successfully";
+    $stmy->close();
+    $conn->close();
+}
+?>
