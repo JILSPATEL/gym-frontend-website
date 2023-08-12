3f5f564e514a1ce4f2463cd79cdb75ba47167271
JILS PATEL <100358865+JILSPATEL@users.noreply.github.com>
2023-08-12 17:20:06 +0530

    GYM.php

--- GYM.php
+++ GYM.php
@@ -1,19 +1,29 @@
 <?php
-    $Name=$_POST['name'];
-    $Age=$_POST['age'];
-    $Address=$_POST['adr'];
-    $Number=$_POST['num'];
-    
-    $conn = new mysqli('localhost','root','','gym_db');
-    if($conn->connect_error){
-        die('connection failed :'+$conn->connect_error);
-}
-else{
-    $stmy=$conn->prepare("insert into gym1(Name,Age,Address,Number) values(?,?,?,?)");
-    $stmy->bind_param("sisi",$Name,$Age,$Address, $Number);
-    $stmy->execute();
-    echo"register successfully";
-    $stmy->close();
-    $conn->close();
+if ($_POST['Submit']) {
+    $Name = $_POST['name'];
+    $Age = $_POST['age'];
+    $Address = $_POST['adr'];
+    $Number = $_POST['num'];
+    $conn = mysqli_connect('localhost', 'root', '', 'gym_db');
+    $query = "INSERT INTO gym1 (Name, Age, Address, Number)
+                VALUES ('$Name', '$Age', '$Address', '$Number')";
+
+    $data = mysqli_query($conn, $query);
+    if ($data) {
+        echo "<script>alert('Data Inserted Succesfully')</script>";
+        ?>
+
+        <meta http-equiv="refresh" content="0; URL=http://localhost/PhpProject1/Gym_Website1/gym.html" />
+        
+        <?php
+    } else {
+        echo "<script>alert('Something Wrong')</script>";
+    }
+} else {
+    echo "<script>alert('Something Wrong')</script>";
 }
 ?>
+
+
+
+
